1c7d75851f16427855ead45e1baf4e5bce719bb0
jmcrego <jmcrego@gmail.com>
2020-11-27 10:57:26 +0100

    models

--- Params.py
+++ Params.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-#import io
-#import os
 import sys
 import logging
 
@@ -17,6 +15,19 @@ def create_logger(logfile, loglevel):
         logging.basicConfig(filename=logfile, format='[%(asctime)s.%(msecs)03d] %(levelname)s %(message)s', datefmt='%Y-%m-%d_%H:%M:%S', level=numeric_level)
         logging.debug('Created Logger level={} file={}'.format(loglevel, logfile))
 
+def read_file_param(file, pars):
+  with open(file) as f: 
+    for l in f:
+      toks = l.rstrip().split(' ')
+      if len(toks) != 2:
+        logging.error('bad parameter entry \'{}\' in file {}'.format(l),file)
+        sys.exit()
+      else:
+        if not pars.read_par('-'+toks[0], toks[1]):
+          logging.error('parameter {} does not allowed in {}'.format(key, pars.__class__.__name__))
+          sys.exit()
+
+
 ##############################################################################################################
 ### network Params ###########################################################################################
 ##############################################################################################################
@@ -29,21 +40,21 @@ class network_params():
     self.ff_dim = 12
     self.n_heads = 8
     self.n_layers = 6
+    self.dropout = 0.1
 
   def usage(self):
     return '''
   Network params
-   -emb_dim : model embedding dimension
-   -qk_dim : query/key dimension
-   -v_dim : value dimension
-   -ff_dim : feed-forward inner layer dimension
-   -n_heads : number of attention heads
-   -n_layers : number of encoder layers
-'''
+   -emb_dim INT : model embedding dimension (4)
+   -qk_dim INT : query/key dimension (2)
+   -v_dim INT : value dimension (2)
+   -ff_dim INT : feed-forward inner layer dimension (12)
+   -n_heads INT : number of attention heads (8)
+   -n_layers FLOAT : number of encoder layers (6)'''
 
   def read_par(self, key, value):
       if key=='-network_params':
-        self.read_file(value)
+        read_file_param(value, self)
         return True
       elif key=='-emb_dim':
         self.emb_dim = int(value)
@@ -63,19 +74,10 @@ class network_params():
       elif key=='-n_layers':
         self.n_layers = int(value)
         return True
+      elif key=='-dropout':
+        self.n_layers = float(value)
+        return True
       return False
-
-  def read_file(self, file):
-    with open(file) as f: 
-      for l in f:
-        toks = l.rstrip().split(' ')
-        if len(toks) != 2:
-          logging.error('bad parameter entry \'{}\' in file {}'.format(l),file)
-          sys.exit()
-        else:
-          if not self.read_par('-'+toks[0], toks[1]):
-            logging.error('parameter {} does not exist in network params')
-            sys.exit()
 
 ##############################################################################################################
 ### optim Params #############################################################################################
@@ -84,33 +86,66 @@ class optim_params():
 
   def __init__(self):
     self.optimizer = 'adam'
+    self.dropout = 0.3
+    self.lr = 2.0
+    self.min_lr = 0.0001
+    self.beta1 = 0.9
+    self.beta2 = 0.998
+    self.eps = 1e-9
+    self.noam_scale = 2.0
+    self.noam_warmup = 4000
+    self.label_smoothing = 0.1
 
   def usage(self):
     return '''
   Optim params
-'''
+   -optimizer STRING : optimization algorithm (adam)
+   -dropout FLOAT : dropout value (0.3)
+   -lr FLOAT : initial learning rate (2.0)
+   -min_lr FLOAT : minimum value for learning rate (0.0001)
+   -beta1 FLOAT : beta1 for adam optimizer (0.9)
+   -beta1 FLOAT : beta2 for adam optimizer (0.998)
+   -eps FLOAT : epsilon for adam optimizer (0.1e-9)
+   -noam_scale : scale of Noam decay for learning rate (2.0)
+   -noam_warmup : warmup steps of Noam decay for learning rate (4000)
+   -label_smoothing: smoothing probability for label smoothing (0.1)'''
 
   def read_par(self, key, value):
       if key=='-optim_params':
-        self.read_file(value)
+        read_file_param(value, self)
         return True
       elif key=='-optimizer':
         self.optimizer = int(value)
         return True
+      elif key=='-dropout':
+        self.dropout = float(value)
+        return True
+      elif key=='-lr':
+        self.lr = float(value)
+        return True
+      elif key=='-min_lr':
+        self.min_lr = float(value)
+        return True
+      elif key=='-beta1':
+        self.beta1 = float(value)
+        return True
+      elif key=='-beta2':
+        self.beta2 = float(value)
+        return True
+      elif key=='-eps':
+        self.eps = float(value)
+        return True
+      elif key=='-noam_scale':
+        self.noam_scale = float(value)
+        return True
+      elif key=='-noam_warmup':
+        self.noam_warmup = float(value)
+        return True
+      elif key=='-label_smoothing':
+        self.label_smoothing = float(value)
+        return True
+
       return False
-
-  def read_file(self, file):
-    with open(file) as f: 
-      for l in f:
-        toks = l.rstrip().split(' ')
-        if len(toks) != 2:
-          logging.error('bad parameter entry \'{}\' in file {}'.format(l),file)
-          sys.exit()
-        else:
-          if not self.read_par('-'+toks[0], toks[1]):
-            logging.error('parameter {} does not exist in optim params')
-            sys.exit()
-
 
 ##############################################################################################################
 ### data Params ##############################################################################################
@@ -123,28 +158,16 @@ class data_params():
   def usage(self):
     return '''
   Data params
-'''
+   -tokenizer STRING : tokenizer (space)'''
 
   def read_par(self, key, value):
       if key=='-data_params':
-        self.read_file(value)
+        self.read_file_param(value, self)
         return True
       elif key=='-tokenizer':
-        self.tokenizer = int(value)
+        self.tokenizer = value
         return True
       return False
-
-  def read_file(self, file):
-    with open(file) as f: 
-      for l in f:
-        toks = l.rstrip().split(' ')
-        if len(toks) != 2:
-          logging.error('bad parameter entry \'{}\' in file {}'.format(l),file)
-          sys.exit()
-        else:
-          if not self.read_par('-'+toks[0], toks[1]):
-            logging.error('parameter {} does not exist in data params')
-            sys.exit()
 
 ##############################################################################################################
 ### learning Params ##########################################################################################
@@ -153,32 +176,31 @@ class learning_params():
 
   def __init__(self):
     self.max_updates = 5000000
+    self.batch_size = 32
+    self.batch_type = 'sentences'    
 
   def usage(self):
     return '''
   Learning params
-'''
+   -max_updates INT : maximum number of training updates (5000000)
+   -batch_size INT : maximum batch size (32)
+   -batch_type STRING : sentences or tokens (sentences)'''
 
   def read_par(self, key, value):
       if key=='-learning_params':
-        self.read_file(value)
+        self.read_file_param(value, self)
         return True
       elif key=='-max_updates':
         self.max_updates = int(value)
         return True
-      return False
+      elif key=='-batch_size':
+        self.batch_size = int(value)
+        return True
+      elif key=='-batch_type':
+        self.batch_type = value
+        return True
 
-  def read_file(self, file):
-    with open(file) as f: 
-      for l in f:
-        toks = l.rstrip().split(' ')
-        if len(toks) != 2:
-          logging.error('bad parameter entry \'{}\' in file {}'.format(l),file)
-          sys.exit()
-        else:
-          if not self.read_par('-'+toks[0], toks[1]):
-            logging.error('parameter {} does not exist in learning params')
-            sys.exit()
+      return False
 
 ##############################################################################################################
 ### inference Params #########################################################################################
@@ -191,28 +213,17 @@ class inference_params():
   def usage(self):
     return '''
   Inference params
-'''
+   -beam INT : size of beam (5)'''
 
   def read_par(self, key, value):
       if key=='-inference_params':
-        self.read_file(value)
+        self.read_file_param(value, self)
         return True
       if key=='-beam':
         self.beam = int(value)
         return True
-      return False
 
-  def read_file(self, file):
-    with open(file) as f: 
-      for l in f:
-        toks = l.rstrip().split(' ')
-        if len(toks) != 2:
-          logging.error('bad parameter entry \'{}\' in file {}'.format(l),file)
-          sys.exit()
-        else:
-          if not self.read_par('-'+toks[0], toks[1]):
-            logging.error('parameter {} does not exist in inference params')
-            sys.exit()
+      return False
 
 ##############################################################################################################
 ### Params ###################################################################################################
@@ -226,6 +237,7 @@ class Params():
     self.learning = learning_params()
     self.inference = inference_params()
     self.prog = argv.pop(0)
+    self.run = None
 
     log_file = None
     log_level = 'info'
@@ -234,6 +246,8 @@ class Params():
       tok = sys.argv.pop(0)
       if tok=="-h":
         self.usage()
+      elif tok=="-run" and len(sys.argv):
+        self.run = sys.argv.pop(0).lower()
       elif tok=="-log_file" and len(sys.argv):
         log_file = sys.argv.pop(0)
       elif tok=="-log_level" and len(sys.argv):
@@ -263,12 +277,12 @@ class Params():
     logging.info("Learning Params = {}".format(self.learning.__dict__))
     logging.info("Inference Params = {}".format(self.inference.__dict__))
 
-
   def usage(self):
-    sys.stderr.write('''usage: {} net_params opt_params data_params learning_params inference_params [-h] [-log_level LEVL] [-log_file FILE]
-  -log_file    FILE : log file  (stderr)
-  -log_level STRING : log level [debug, info, warning, critical, error] (info)
-  -h                : this help
+    sys.stderr.write('''usage: {} -run COMMAND [net_params] [opt_params] [data_params] [learning_params] [inference_params] [-h] [-log_level LEVL] [-log_file FILE]
+   -run COMMAND : learn or inference
+   -log_file FILE : log file  (stderr)
+   -log_level STRING : log level [debug, info, warning, critical, error] (info)
+   -h                : this help
 {}
 {}
 {}
--- Transformer.py
+++ Transformer.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-#import io
-#import os
 import sys
 import time
 import logging
@@ -15,6 +13,15 @@ if __name__ == '__main__':
 
     pars = Params(sys.argv)
     tic = time.time()
+
+    if pars.run == 'learning':
+      logging.info('Running: learning')
+
+    elif pars.run == 'inference':
+      logging.info('Running: inference')
+
+    else:
+      logging.warning('Nothing to run')
 
     toc = time.time()
     logging.info('Done ({:.3f} seconds)'.format(toc-tic))
