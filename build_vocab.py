d93591f39d36ee99b91476952831b65ac5b97ec3
jmcrego <jmcrego@gmail.com>
2020-11-28 20:46:31 +0100

    light

--- build_vocab.py
+++ build_vocab.py
@@ -5,21 +5,44 @@ import time
 import logging
 from Data import Vocab
 
+def create_logger(logfile, loglevel):
+    numeric_level = getattr(logging, loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        logging.error("Invalid log level={}".format(loglevel))
+        sys.exit()
+    if logfile is None or logfile == 'stderr':
+        logging.basicConfig(format='[%(asctime)s.%(msecs)03d] %(levelname)s %(message)s', datefmt='%Y-%m-%d_%H:%M:%S', level=numeric_level)
+        logging.debug('Created Logger level={}'.format(loglevel))
+    else:
+        logging.basicConfig(filename=logfile, format='[%(asctime)s.%(msecs)03d] %(levelname)s %(message)s', datefmt='%Y-%m-%d_%H:%M:%S', level=numeric_level)
+        logging.debug('Created Logger level={} file={}'.format(loglevel, logfile))
+
 ######################################################################
 ### MAIN #############################################################
 ######################################################################
             
 if __name__ == '__main__':
-    prog = sys.argv.pop(0)
-    if len(sys.argv) == 0:
-        sys.stderr.write('usage: {} ONMT_TOKENIZER_CONFIG < text > vocab\n'.format(prog))
-        sys.exit()
 
+    prog = sys.argv.pop(0)
+    usage = '''usage: {} [-tokenizer_config FILE] < text > vocab
+   -tokenizer_config FILE : tokenizer config file (if not used mode=space)
+'''.format(prog)
+
+    ftokconf = None
+    while len(sys.argv):
+        tok = sys.argv.pop(0)
+        if tok=="-h":
+            sys.stderr.write(usage);
+            sys.exit()
+        elif tok=="-tokenizer_config":
+            ftokconf = sys.argv.pop(0)
+
+    create_logger(None, 'info')
     tic = time.time()
     voc = Vocab()
-    voc.build(sys.argv[0])
+    voc.build(ftokconf)
     toc = time.time()
-    sys.stderr.write('Done ({:.3f} seconds)\n'.format(toc-tic))
+    logging.info('Done ({:.3f} seconds)'.format(toc-tic))
 
 
 
