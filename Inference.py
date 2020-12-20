2dbdd890993bd88267483dbc63feb33a76bc885b
jmcrego <jmcrego@gmail.com>
2020-12-20 18:30:31 +0100

    inference

--- Inference.py
+++ Inference.py
@@ -172,15 +172,13 @@ class BeamSearch():
       logging.info('kbest_logP = {} kbest_hyps = {}'.format(kbest_logP.shape, kbest_hyps.shape))
       for h in range(len(kbest_hyps)):
         k = kbest_hyps[h]
-        wrds = curr_hyps[k]
-        logP = curr_logP[k]
-        cost = sum(logP[k])
+        cost = sum(curr_logP[k])
         sys.stdout.write('step:{} batch:{} hyp:{} cost:{:.5f} |||'.format(lt,b,h,cost))
-        for i in range(len(wrds)):
-          idx = wrds[i].item()
-          cst = logP[i].item()
+        for i in range(len(curr_hyps[k])):
+          idx = curr_hyps[k,i].item()
           wrd = self.tgt_vocab[idx]
-          sys.stdout.write(' {}:{}:{:.5f}'.format(idx, wrd, cst))
+          logP = curr_logP[k,i].item()
+          sys.stdout.write(' {}:{}:{:.5f}'.format(idx, wrd, logP))
         print()
 
 
