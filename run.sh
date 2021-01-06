bc3325f70ce7501f12216700ca098ce1b18e259b
jmcrego <jmcrego@gmail.com>
2021-01-06 12:15:10 +0100

    light

--- run.sh
+++ run.sh
@@ -14,20 +14,17 @@ preprocess(){
     cat $dir/train.{en,fr} | python3 ./learnBPE_cli.py $fbpe #joint bpe
     cat $dir/train.en | python3 buildvoc_cli.py -tokenizer_config $ftok -max_size 32768 > $voc_ss
     cat $dir/train.fr | python3 buildvoc_cli.py -tokenizer_config $ftok -max_size 32768 > $voc_tt
+    python3 ./word2idx_cli.py -src $dir/valid.en -tgt $dir/valid.fr -voc_src $voc_ss -voc_tgt $voc_tt -tok_src $ftok -tok_tgt $ftok -set $valid
+    python3 ./word2idx_cli.py -src $dir/train.en -tgt $dir/train.fr -voc_src $voc_ss -voc_tgt $voc_tt -tok_src $ftok -tok_tgt $ftok -set $train
+    python3 ./word2idx_cli.py -src $dir/test.en -voc_src $voc_ss -tok_src $ftok -set $test
 }
 
 train(){
-    train=$dir/bin/train.bin
-    valid=$dir/bin/valid.bin
-    #python3 ./word2idx_cli.py -src $dir/valid.en -tgt $dir/valid.fr -voc_src $voc_ss -voc_tgt $voc_tt -tok_src $ftok -tok_tgt $ftok -set $valid
-    #python3 ./word2idx_cli.py -src $dir/train.en -tgt $dir/train.fr -voc_src $voc_ss -voc_tgt $voc_tt -tok_src $ftok -tok_tgt $ftok -set $train
-    python3 ./Transformer.py -suffix $suffix -src_vocab $voc_ss -tgt_vocab $voc_tt -train_set $train -valid_set $valid -batch_size 16 -batch_type sentences -report_every 5 -save_every 100 -log_level info
+    python3 ./Transformer.py -suffix $suffix -src_vocab $voc_ss -tgt_vocab $voc_tt -train_set $train -valid_set $valid -batch_size 16 -batch_type sentences -report_every 5 -save_every 100
 }
 
 inference(){
-    test=$dir/bin/test.en.bin
-    #python3 ./word2idx_cli.py -src $dir/test.en -voc_src $voc_ss -tok_src $ftok -set $test
-    python3 ./Transformer.py -suffix $suffix -src_vocab $voc_ss -tgt_vocab $voc_tt -test_set $test -batch_size 3 -batch_type sentences -beam_size 4 -n_best 2 -max_size 15 -log_level info 
+    python3 ./Transformer.py -suffix $suffix -src_vocab $voc_ss -tgt_vocab $voc_tt -test_set $test -batch_size 3 -batch_type sentences -beam_size 4 -n_best 2 -max_size 15
 }
 
 dir=$PWD/files
@@ -37,6 +34,9 @@ ftok=$dir/bin/tokconf
 voc_ss=$dir/bin/en.vocab
 voc_tt=$dir/bin/fr.vocab
 suffix=$dir/bin/model
+train=$dir/bin/train-enfr.bin
+valid=$dir/bin/valid-enfr.bin
+test=$dir/bin/test-en.bin
 
 #data
 #preprocess
