d5b475b7ef51f949727f7fca23bb7b02b23c67e6
jmcrego <jmcrego@gmail.com>
2021-01-11 14:18:07 +0100

    ligth

--- transformer/Inference.py
+++ transformer/Inference.py
@@ -118,7 +118,6 @@ class Beam():
     lt = self.hyps.shape[1]
     print('[{}] hyps.size={}'.format(tag, self.hyps.shape[1]))    
     b=0
-    k=0
     for i in range(self.hyps.shape[0]):
       sum_logP_norm = sum(self.logP[i]) / norm_length(lt,self.alpha)
       #
@@ -128,11 +127,8 @@ class Beam():
       toks1 = ["{}".format(self.tgt_vocab[self.hyps[i,j].item()]) for j in range(len(self.hyps[i]))]
       toks2 = ["{:.4f}".format(self.logP[i,j].item()) for j in range(len(self.hyps[i]))]
       print('i={} b={}\t{:.5f}\t{}\t{}'.format(i,b,sum_logP_norm,' '.join(toks1),' '.join(toks2)))
-      if b==self.K:
-        k+=1
-        b=0
-      else:
-        b += 1
+      if i%self.K == 0:
+        b=+1
 
 
 
