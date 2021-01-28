2599bf934bc62d5c2f20018b241383fc4c3f7f88
jmcrego <jmcrego@gmail.com>
2021-01-28 09:11:04 +0100

    light

--- figures.py
+++ figures.py
@@ -11,68 +11,68 @@ from transformer.Model import prepare_source, prepare_target
 
 
 def plotPoints2d(X,Y,xlabel=None,ylabel=None,legend=None,f=None):
-  plt.figure(figsize=(15, 5))
-  plt.plot(X,Y)
-  if xlabel is not None:
-    plt.xlabel(xlabel)
-  if ylabel is not None:
-    plt.ylabel(ylabel)
-  if legend is not None:
-    plt.legend(legend)
-  if f is not None:
-    plt.savefig(f)
-  plt.grid(True)
-  plt.show()
+	plt.figure(figsize=(15, 5))
+	plt.plot(X,Y)
+	if xlabel is not None:
+		plt.xlabel(xlabel)
+	if ylabel is not None:
+		plt.ylabel(ylabel)
+	if legend is not None:
+		plt.legend(legend)
+	if f is not None:
+		plt.savefig(f)
+	plt.grid(True)
+	plt.show()
 
 def plotMatrix2d(X,f=None):
-  #print(X.shape)
-  #print(X)
-  plt.figure(figsize=(10, 10))
-  if f is not None:
-    plt.savefig(f)
-  plt.imshow(X)
+	#print(X.shape)
+	#print(X)
+	plt.figure(figsize=(10, 10))
+	if f is not None:
+		plt.savefig(f)
+	plt.imshow(X)
 
 def plotPositionalEncoding():
-  bs = 1
-  ls = 100
-  ed = 20
-  plt.figure(figsize=(20, 5))
-  pe = AddPositionalEncoding(ed, 0.0)               #ed, dropout
-  y = pe.forward(Variable(torch.zeros(bs, ls, ed))) #[bs, ls, ed]
-  y = y[0, :, 4:8].data.numpy()                     #[ls, 4]
-  plt.plot(np.arange(ls), y)
-  plt.grid(True)
-  plt.legend(["dim %d"%p for p in [4,5,6,7]])
-  plt.show()
+	bs = 1
+	ls = 100
+	ed = 20
+	plt.figure(figsize=(20, 5))
+	pe = AddPositionalEncoding(ed, 0.0)               #ed, dropout
+	y = pe.forward(Variable(torch.zeros(bs, ls, ed))) #[bs, ls, ed]
+	y = y[0, :, 4:8].data.numpy()                     #[ls, 4]
+	plt.plot(np.arange(ls), y)
+	plt.grid(True)
+	plt.legend(["dim %d"%p for p in [4,5,6,7]])
+	plt.show()
 
 def plotLRate(N):
-  emb_dim = 256
-  noam_scale = 2.0
-  noam_warmup = 4000
-  lr = 2.0
-  beta1 = 0.9
-  beta2 = 0.998
-  eps = 1e-9
-  n_layers = 6
-  n_heads = 8
-  ff_dim = 1024
-  qk_dim = 64
-  v_dim = 64
-  dropout = 0.1
-  Vs = 32000
-  Vt = 32000
-  idx_pad = 0
-  model = Encoder_Decoder(n_layers, ff_dim, n_heads, emb_dim, qk_dim, v_dim, dropout, Vs, Vt, idx_pad)
-  optim = torch.optim.Adam(model.parameters(), lr=lr, betas=(beta1, beta2), eps=eps)
-  optScheduler = OptScheduler(optim, emb_dim, noam_scale, noam_warmup, 0)
-  X = [i for i in range(1,N)]
-  Y = [optScheduler.lrate(i) for i in range(1,N)]
-  xlabel = '#Iter'
-  ylabel = 'LRate'
-  legend = ["dim={} scale={:.2f} warmup={}".format(emb_dim,noam_scale,noam_warmup)]
-  file = None
-  plotPoints2d(X, Y, xlabel=xlabel, ylabel=ylabel, legend=legend, f=file)
-  
+	emb_dim = 256
+	noam_scale = 2.0
+	noam_warmup = 4000
+	lr = 2.0
+	beta1 = 0.9
+	beta2 = 0.998
+	eps = 1e-9
+	n_layers = 6
+	n_heads = 8
+	ff_dim = 1024
+	qk_dim = 64
+	v_dim = 64
+	dropout = 0.1
+	Vs = 32000
+	Vt = 32000
+	idx_pad = 0
+	model = Encoder_Decoder(n_layers, ff_dim, n_heads, emb_dim, qk_dim, v_dim, dropout, Vs, Vt, idx_pad)
+	optim = torch.optim.Adam(model.parameters(), lr=lr, betas=(beta1, beta2), eps=eps)
+	optScheduler = OptScheduler(optim, emb_dim, noam_scale, noam_warmup, 0)
+	X = [i for i in range(1,N)]
+	Y = [optScheduler.lrate(i) for i in range(1,N)]
+	xlabel = '#Iter'
+	ylabel = 'LRate'
+	legend = ["dim={} scale={:.2f} warmup={}".format(emb_dim,noam_scale,noam_warmup)]
+	file = None
+	plotPoints2d(X, Y, xlabel=xlabel, ylabel=ylabel, legend=legend, f=file)
+	
 
 def plotCurves(file):
 	step_s = []
@@ -82,18 +82,23 @@ def plotCurves(file):
 	vloss_s = []
 	with open(file,'r') as f: 
 		for l in f:
-    	#[2021-01-12_12:07:11.086] INFO Learning step:363700 epoch:18 batch:4813/21111 ms/batch:213.41 lr:0.000147 loss/tok:2.486
+			#[2021-01-12_12:07:11.086] INFO Learning step:363700 epoch:18 batch:4813/21111 ms/batch:213.41 lr:0.000147 loss/tok:2.486
+			#INFO:tensorflow:Step = 500 ; steps/s = 4.81, source words/s = 16172, target words/s = 19413 ; Learning rate = 0.000175 ; Loss = 5.969543
 			step = None
 			lr = None
 			loss = None
 			toks = l.rstrip().split()
-			if toks[2] == 'LearningStep:' and toks[12] == 'lr:' and toks[14] == 'loss:':
+			if len(toks) > 14 and toks[2] == 'LearningStep:' and toks[12] == 'lr:' and toks[14] == 'loss:':
 				step_s.append(int(toks[3]))
 				lr_s.append(float(toks[13]))
 				loss_s.append(float(toks[15]))
-			elif toks[2] == 'Validation' and toks[3] == 'LearningStep:' and toks[9] == 'loss:':
+			elif len(toks) > 9 and toks[2] == 'Validation' and toks[3] == 'LearningStep:' and toks[9] == 'loss:':
 				vstep_s.append(int(toks[4]))
 				vloss_s.append(float(toks[10]))
+			elif len(toks) > 19 and toks[16] == 'Learning':
+				lr_s.append(float(toks[19]))
+				step_s.append(int(toks[2]))
+				loss_s.append(float(toks[-1]))
 
 	plt.figure(figsize=(20, 10))
 
@@ -143,7 +148,7 @@ def plotMasks():
 
 if __name__ == '__main__':
 
-  #plotPositionalEncoding()
-  #plotLRate(1000000)
-  #plotMasks()
-  plotCurves(sys.argv[1])
+	#plotPositionalEncoding()
+	#plotLRate(1000000)
+	#plotMasks()
+	plotCurves(sys.argv[1])
