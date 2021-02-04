f608010b140aac9c357cffdb4d1d0f4f383bdc15
jmcrego <jmcrego@gmail.com>
2021-02-04 10:36:50 +0100

    light MultiHead_Attn rewrites

--- transformer/Model.py
+++ transformer/Model.py
@@ -307,10 +307,12 @@ class MultiHead_Attn(torch.nn.Module):
     Q = Q / math.sqrt(self.kd)
     s = torch.matmul(Q, K.transpose(2, 3)) #[bs,nh,lq,qd] x [bs,nh,kd,lk] = [bs,nh,lq,lk] # thanks to qd==kd #in decoder lq are target words and lk are source words
     if msk is not None:
-      s = s.masked_fill(msk == 0, -1e9) #score=-1e9 to masked tokens
-    w = self.dropout(torch.nn.functional.softmax(s, dim=-1)) #[bs,nh,lq,lk] (these are the attention weights)
+      s = s.masked_fill(msk == 0, float('-inf')) #score=-Inf to masked tokens
+    w = torch.nn.functional.softmax(s, dim=-1) #[bs,nh,lq,lk] (these are the attention weights)
+    w = self.dropout(w) #[bs,nh,lq,lk] 
+
     z = torch.matmul(w,V) #[bs,nh,lq,lk] x [bs,nh,lv,vd] = [bs,nh,lq,vd] #thanks to lk==lv
-    z = z.transpose(1,2).contiguous().view([bs,lq,-1]) #=> [bs,lq,nh,vd] => [bs,lq,nh*vd]
+    z = z.transpose(1,2).contiguous().view([bs,lq,self.nh*self.vd]) #=> [bs,lq,nh,vd] => [bs,lq,nh*vd]
     z = self.WO(z) #[bs,lq,ed]
     return self.dropout(z)
 
