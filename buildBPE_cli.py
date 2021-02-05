befc0bbb7b20ea5f9b45abe4877b562ba44bd444
jmcrego <jmcrego@gmail.com>
2021-02-05 10:32:47 +0100

    rewrite

--- buildBPE_cli.py
+++ buildBPE_cli.py
@@ -2,10 +2,24 @@
 import pyonmttok
 import sys
 import yaml
+import logging
+
+def create_logger(logfile, loglevel):
+    numeric_level = getattr(logging, loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        logging.error("Invalid log level={}".format(loglevel))
+        sys.exit()
+    if logfile is None or logfile == 'stderr':
+        logging.basicConfig(format='[%(asctime)s.%(msecs)03d] %(levelname)s %(message)s', datefmt='%Y-%m-%d_%H:%M:%S', level=numeric_level)
+        logging.debug('Created Logger level={}'.format(loglevel))
+    else:
+        logging.basicConfig(filename=logfile, format='[%(asctime)s.%(msecs)03d] %(levelname)s %(message)s', datefmt='%Y-%m-%d_%H:%M:%S', level=numeric_level)
+        logging.debug('Created Logger level={} file={}'.format(loglevel, logfile))
 
 symbols = 30000
 tok_config = None
 bpe_model = None
+log_level = 'info'
 prog = sys.argv.pop(0)
 usage = '''usage: {} -tok_config FILE -bpe_model FILE [-symobls INT]
 	-tok_config FILE : tokenization config file
@@ -24,19 +38,21 @@ while len(sys.argv):
 		bpe_model = sys.argv.pop(0)
 	elif tok=='-symbols' and len(sys.argv)>=0:
 		symbols = int(sys.argv.pop(0))
+	elif tok=="-log_level":
+		log_level = sys.argv.pop(0)
 	else:
 		sys.stderr.write('error: unparsed {} option\n'.format(tok))
 		sys.stderr.write(usage)
 		sys.exit()
 
+create_logger(None, log_level)
+
 if tok_config is None:		
-	sys.stderr.write('error: missing -tok_config option\n')
-	sys.stderr.write(usage)
+	logging.error('error: missing -tok_config option')
 	sys.exit()
 
 if bpe_model is None:		
-	sys.stderr.write('error: missing -bpe_model option\n')
-	sys.stderr.write(usage)
+	logging.error('error: missing -bpe_model option')
 	sys.exit()
 
 ###
@@ -49,7 +65,7 @@ if 'mode' not in opts:
 	sys.exit()
 mode = opts['mode']
 del opts["mode"]
-sys.stderr.write('Using tokenization mode: {} opts: {}\n'.format(mode, opts))
+logging.info('Tokenization mode: {} opts: {}'.format(mode, opts))
 
 ###
 ### LEARN BPE model
@@ -58,15 +74,17 @@ tokenizer = pyonmttok.Tokenizer(mode, **opts)
 learner = pyonmttok.BPELearner(tokenizer=tokenizer, symbols=symbols)
 for l in sys.stdin:
   learner.ingest(l)
-sys.stderr.write('learning bpe model: {}\n'.format(bpe_model))
+loging.info('Learning bpe model: {}'.format(bpe_model))
 tokenizer = learner.learn(bpe_model)
 
 ###
-### CONFIG FILE
+### OUTPUT config file
 ###
 opts['mode'] = mode
 opts['bpe_model_path'] = bpe_model
 with open('{}.tok_config'.format(bpe_model), "w") as yamlfile:
 	yaml.dump(opts, yamlfile)
 
-sys.stderr.write('Built {} and {}.tok_config files\n'.format(bpe_model,bpe_model))
+logging.info('Built {} and {}.tok_config files'.format(bpe_model,bpe_model))
+
+
