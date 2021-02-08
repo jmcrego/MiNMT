ec4c497f211c9b37f3add77a57cd4a8aa2493758
jmcrego <jmcrego@gmail.com>
2021-02-08 22:59:30 +0100

    light

--- tokenize_sp.py
+++ tokenize_sp.py
@@ -5,15 +5,15 @@ import logging
 
 def create_logger(logfile, loglevel):
   numeric_level = getattr(logging, loglevel.upper(), None)
-    if not isinstance(numeric_level, int):
-      logging.error("Invalid log level={}".format(loglevel))
-      sys.exit()
-    if logfile is None or logfile == 'stderr':
-      logging.basicConfig(format='[%(asctime)s.%(msecs)03d] %(levelname)s %(message)s', datefmt='%Y-%m-%d_%H:%M:%S', level=numeric_level)
-      logging.debug('Created Logger level={}'.format(loglevel))
-    else:
-      logging.basicConfig(filename=logfile, format='[%(asctime)s.%(msecs)03d] %(levelname)s %(message)s', datefmt='%Y-%m-%d_%H:%M:%S', level=numeric_level)
-      logging.debug('Created Logger level={} file={}'.format(loglevel, logfile))
+  if not isinstance(numeric_level, int):
+    logging.error("Invalid log level={}".format(loglevel))
+    sys.exit()
+  if logfile is None or logfile == 'stderr':
+    logging.basicConfig(format='[%(asctime)s.%(msecs)03d] %(levelname)s %(message)s', datefmt='%Y-%m-%d_%H:%M:%S', level=numeric_level)
+    logging.debug('Created Logger level={}'.format(loglevel))
+  else:
+    logging.basicConfig(filename=logfile, format='[%(asctime)s.%(msecs)03d] %(levelname)s %(message)s', datefmt='%Y-%m-%d_%H:%M:%S', level=numeric_level)
+    logging.debug('Created Logger level={} file={}'.format(loglevel, logfile))
 
 sp_model = None
 log_level = 'info'
