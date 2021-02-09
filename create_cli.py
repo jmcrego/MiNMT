3349dc5537a570309e67e535ed44649e416b9092
jmcrego <jmcrego@gmail.com>
2021-02-09 13:38:46 +0100

    light

--- create_cli.py
+++ create_cli.py
@@ -97,7 +97,7 @@ class Options():
       self.usage()
 
   def usage(self):
-    sys.stderr.write('''{} -dnet FILE [Options]
+    sys.stderr.write('''usage: {} -dnet FILE [Options]
    -dnet         DIR : network ouput directory [must not exist]
    -src_vocab   FILE : source SentencePiece vocab
    -tgt_vocab   FILE : target SentencePiece vocab
