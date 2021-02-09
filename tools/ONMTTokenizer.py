31224a1046fb614c3d6709095d9e4fd9f5bb0c06
jmcrego <jmcrego@gmail.com>
2021-02-09 17:00:29 +0100

    light

--- tools/ONMTTokenizer.py
+++ tools/ONMTTokenizer.py
@@ -4,14 +4,14 @@ import sys
 import pyonmttok
 
 class ONMTTokenizer():
-  def __init__(self, sp_model=None):
+	def __init__(self, sp_model=None):
   	if sp_model is not None:
 	    self.tokenizer = pyonmttok.Tokenizer(mode = 'space', sp_model_path = sp_model, joiner_annotate = True)
-	else:
-	    self.tokenizer = pyonmttok.Tokenizer(mode = 'space', joiner_annotate = True)
+		else:
+			self.tokenizer = pyonmttok.Tokenizer(mode = 'space', joiner_annotate = True)
 
-  def tokenize(self, text):
-    return self.tokenizer.tokenize(text)[0]
+	def tokenize(self, text):
+		return self.tokenizer.tokenize(text)[0]
 
-  def detokenize(self, tokens):
-    return self.tokenizer.detokenize(tokens)
+	def detokenize(self, tokens):
+		return self.tokenizer.detokenize(tokens)
