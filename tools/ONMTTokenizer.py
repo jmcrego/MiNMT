bc023d3508ccb10ee19abff3c9c6ff43c75dcc34
jmcrego <jmcrego@gmail.com>
2021-02-09 16:59:11 +0100

    add

--- tools/ONMTTokenizer.py
+++ tools/ONMTTokenizer.py
@@ -2,18 +2,16 @@
 
 import sys
 import pyonmttok
-#import logging
 
-####################################################################
-### ONMTTokenizer ##################################################
-####################################################################
 class ONMTTokenizer():
-  def __init__(self, sp_model):
-    self.tokenizer = pyonmttok.Tokenizer(mode = 'space', sp_model_path = sp_model, joiner_annotate = True)
+  def __init__(self, sp_model=None):
+  	if sp_model is not None:
+	    self.tokenizer = pyonmttok.Tokenizer(mode = 'space', sp_model_path = sp_model, joiner_annotate = True)
+	else:
+	    self.tokenizer = pyonmttok.Tokenizer(mode = 'space', joiner_annotate = True)
 
   def tokenize(self, text):
     return self.tokenizer.tokenize(text)[0]
 
   def detokenize(self, tokens):
     return self.tokenizer.detokenize(tokens)
-
