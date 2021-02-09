7c35438f1678faf0f30adb268f50ada2384874f7
jmcrego <jmcrego@gmail.com>
2021-02-09 21:02:02 +0100

    light

--- tools/ONMTTokenizer.py
+++ tools/ONMTTokenizer.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import logging
 import pyonmttok
 
 class ONMTTokenizer():
 	def __init__(self, sp_model=None):
 		if sp_model is None or not os.path.exists(sp_model):
 			self.tokenizer = pyonmttok.Tokenizer(mode = 'space', spacer_annotate = True)
+			logging.info('SPACE tokenizer')
 		else:
 			self.tokenizer = pyonmttok.Tokenizer(mode = 'none', spacer_annotate = True, sp_model_path = sp_model)
+			logging.info('SentencePiece tokenizer')
 
 	def tokenize(self, text):
 		return self.tokenizer.tokenize(text)[0]
