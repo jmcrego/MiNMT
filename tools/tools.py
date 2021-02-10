82ea19151b45099afe97f0fb743ef1ed466aedb7
jmcrego <jmcrego@gmail.com>
2021-02-10 10:56:35 +0100

    add

---
+++ tools/tools.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import logging
+
+def create_logger(logfile, loglevel):
+  numeric_level = getattr(logging, loglevel.upper(), None)
+  if not isinstance(numeric_level, int):
+    logging.error("Invalid log level={}".format(loglevel))
+    sys.exit()
+  if logfile is None or logfile == 'stderr':
+    logging.basicConfig(format='[%(asctime)s.%(msecs)03d] %(levelname)s %(message)s', datefmt='%Y-%m-%d_%H:%M:%S', level=numeric_level)
+    logging.debug('Created Logger level={}'.format(loglevel))
+  else:
+    logging.basicConfig(filename=logfile, format='[%(asctime)s.%(msecs)03d] %(levelname)s %(message)s', datefmt='%Y-%m-%d_%H:%M:%S', level=numeric_level)
+    logging.debug('Created Logger level={} file={}'.format(loglevel, logfile))
+
+
+
