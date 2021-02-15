746b01d42a94b439b620b66e07fdb1e777b06d1b
jmcrego <jmcrego@gmail.com>
2021-02-15 11:05:25 +0100

    add

---
+++ tools/spm_decode.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+import glob
+from tools.SentencePiece import SentencePiece
+from tools.tools import create_logger
+
+if __name__ == '__main__':
+
+  sp_model = None
+  fin = None
+  in_type = 'str'
+  prog = sys.argv.pop(0)
+  usage = '''usage: {} -sp_model FILE [-i FILE]
+   -sp_model  FILE : input model/vocab prefix
+   -i         FILE : input file (default stdin)
+   -in_type   TYPE : str OR int (default str)
+   -h              : this help
+Visit: https://github.com/google/sentencepiece/blob/master/python/README.md
+'''.format(prog)
+
+  while len(sys.argv):
+    tok = sys.argv.pop(0)
+    if tok=='-h':
+      sys.stderr.write(usage)
+      sys.exit()
+    elif tok=='-i' and len(sys.argv)>=0:
+      fin = sys.argv.pop(0)
+    elif tok=='-sp_model' and len(sys.argv)>=0:
+      sp_model = sys.argv.pop(0)
+    elif tok=='-in_type' and len(sys.argv)>=0:
+      in_type = sys.argv.pop(0)
+    else:
+      sys.stderr.write('error: unparsed {} option\n'.format(tok))
+      sys.stderr.write(usage)
+      sys.exit()
+
+  if sp_model is None or not os.path.exists(sp_model):
+    sys.stderr.write('error: missing or unreachable -sp_model file\n')
+    sys.exit()
+
+  create_logger('stderr','info')
+  sp = SentencePiece(sp_model=sp_model)
+  lines = sp.decode(fin=fin, in_type=in_type)
+  for l in lines:
+    print(l)
---
+++ tools/spm_train.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+from tools.SentencePiece import SentencePiece
+from tools.tools import create_logger
+
+if __name__ == '__main__':
+
+  vocab_size = 30000
+  sp_model = None
+  fins = []
+  prog = sys.argv.pop(0)
+  usage = '''usage: {} -sp_model FILE -i FILE+ [-vocab_size INT]
+   -i         FILE : input file/s (multiple files and wildcards allowed)
+   -sp_model  FILE : output model/vocab prefix
+   -vocab_size INT : vocabulary size ({})
+   -h              : this help
+Visit: https://github.com/google/sentencepiece/blob/master/doc/options.md
+'''.format(prog,vocab_size)
+
+  while len(sys.argv):
+    tok = sys.argv.pop(0)
+    if tok=='-h':
+      sys.stderr.write(usage)
+      sys.exit()
+    elif tok=='-i' and len(sys.argv)>=0:
+      while len(sys.argv)>0 and not sys.argv[0].startswith('-'):
+        fins.append(sys.argv.pop(0))
+    elif tok=='-sp_model' and len(sys.argv)>=0:
+      sp_model = sys.argv.pop(0)
+    elif tok=='-vocab_size' and len(sys.argv)>=0:
+      vocab_size = int(sys.argv.pop(0))
+    else:
+      sys.stderr.write('error: unparsed {} option\n'.format(tok))
+      sys.stderr.write(usage)
+      sys.exit()
+
+  if len(fins)==0:
+    sys.stderr.write('error: missing -i option\n')
+    sys.exit()
+
+  if sp_model is None:
+    sys.stderr.write('error: missing -sp_model option\n')
+    sys.exit()
+
+  create_logger('stderr','info')
+  sp = SentencePiece(sp_model=None)
+  sp.train(sp_model=sp_model, fins=fins, vocab_size=vocab_size)
