914e07ec8eaddfdf47b443b9320a7fd438d61254
jmcrego <jmcrego@gmail.com>
2021-02-18 19:13:44 +0100

    noPE

--- transformer/Model.py
+++ transformer/Model.py
@@ -187,32 +187,6 @@ class AddPositionalEncoding(torch.nn.Module):
     x = x + self.pe[:, :l, :] #[bs, l, ed] + [1, l, ed] => [bs, l, ed]
     return self.dropout(x)
 
-
-
-
-class PositionalEncoding(nn.Module):
-    def __init__(self, dropout, dim, max_len=5000):
-        if dim % 2 != 0: raise ValueError("Cannot use sin/cos positional encoding with odd dim (got dim={:d})".format(dim))
-        pe = torch.zeros(max_len, dim)
-        position = torch.arange(0, max_len).unsqueeze(1)
-        div_term = torch.exp((torch.arange(0, dim, 2, dtype=torch.float) * -(math.log(10000.0) / dim)))
-        pe[:, 0::2] = torch.sin(position.float() * div_term)
-        pe[:, 1::2] = torch.cos(position.float() * div_term)
-        pe = pe.unsqueeze(1)
-
-        super(PositionalEncoding, self).__init__()
-        self.register_buffer('pe', pe)
-
-        self.dropout = nn.Dropout(p=dropout)
-        self.dim = dim
-
-    def forward(self, emb, step=None):
-        emb = emb * math.sqrt(self.dim)
-        step = step or 0
-        emb = emb + self.pe[step:emb.size(0)+step]
-        emb = self.dropout(emb)
-        return emb
-
 ##############################################################################################################
 ### Stacked_Encoder ##########################################################################################
 ##############################################################################################################
