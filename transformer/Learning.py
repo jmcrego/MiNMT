d1c4a5f87746aba8d84674ab75966a78a72e1a40
jmcrego <jmcrego@gmail.com>
2021-02-18 14:16:17 +0100

    light

--- transformer/Learning.py
+++ transformer/Learning.py
@@ -19,20 +19,39 @@ class Score():
     self.nsteps = 0
     self.loss = 0.
     self.ntoks = 0
+    self.nok = 0
     self.msec_epoch = time.time()
     #report
     self.loss_report = 0.
+    self.nok_report = 0
     self.ntoks_report = 0
     self.nsteps_report = 0
     self.msec_report = self.msec_epoch
 
-  def step(self, sum_loss_batch, ntoks_batch):
+  def Acc(self, gold, pred, idx_pad):
+    hyps = torch.nn.functional.log_softmax(pred, dim=-1) #[bs*lt, Vt]
+    _, inds = torch.topk(hyps, k=1, dim=-1) #[bs*lt,1]
+    inds = inds.squeeze(-1) #[bs*lt]
+    gold_nopad = torch.tensor(gold != idx_pad)
+    pred_is_gold = torch.tensor(gold == inds)
+    nok = torch.sum(torch.logical_and(gold_nopad, pred_is_gold))
+    ntoks = torch.sum(gold_nopad)
+    return nok.item(), ntoks.item()
+
+  def step(self, sum_loss_batch, gold, pred, idx_pad):
+    #gold is [bs, lt]
+    #pred is [bs, lt, Vt]
+    gold = gold.contiguous().view(-1) #[bs*lt]
+    pred = pred.contiguous().view(-1,pred.size(2)) #[bs*lt, Vt]
+    nok_batch, ntoks_batch = self.Acc(gold,pred,idx_pad)
     #global
     self.nsteps += 1
     self.loss += sum_loss_batch
+    self.nok += nok_batch
     self.ntoks += ntoks_batch
     #report
     self.loss_report += sum_loss_batch
+    self.nok_report += nok_batch
     self.ntoks_report += ntoks_batch
     self.nsteps_report += 1
 
@@ -41,29 +60,34 @@ class Score():
     if self.ntoks_report and self.nsteps_report:
       #print('Report loss={:.5f} ntoks={}'.format(self.loss_report, self.ntoks_report))
       loss_per_tok = self.loss_report / (1.0*self.ntoks_report)
+      acc_per_tok = self.nok_report / (1.0*self.ntoks_report)
       ms_per_step = 1000.0 * (tnow - self.msec_report) / (1.0*self.nsteps_report)
     else:
       loss_per_tok = 0.
+      acc_per_tok = 0.
       ms_per_step = 0.
       logging.warning('Requested report after 0 tokens optimised')
     #initialize for next report
     self.loss_report = 0.
     self.ntoks_report = 0
+    self.nok_report = 0
     self.nsteps_report = 0
     self.msec_report = tnow
-    return loss_per_tok, ms_per_step
+    return acc_per_tok, loss_per_tok, ms_per_step
 
   def epoch(self):
     tnow = time.time()
     if self.ntoks and self.nsteps:
       loss_per_tok = self.loss / (1.0*self.ntoks)
+      acc_per_tok = self.nok / (1.0*self.ntoks)
       ms_epoch = 1000.0 * (tnow - self.msec_epoch)
     else:
       loss_per_tok = 0.
+      acc_per_tok = 0.
       ms_epoch = 0.
       logging.warning('Requested epoch report after 0 tokens optimised')
     #no need to initialize
-    return loss_per_tok, ms_epoch
+    return acc_per_tok, loss_per_tok, ms_epoch
 
 ##############################################################################################################
 ### Learning #################################################################################################
@@ -112,11 +136,11 @@ class Learning():
           torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.clip_grad_norm) ### clip gradients to clip_grad_norm
         self.optScheduler.step()                                                       ### updates model parameters after incrementing step and updating lr
         ### accumulate score
-        score.step(loss_batch.item(), torch.sum(ref != self.idx_pad))
+        score.step(loss_batch.item(), ref, pred, self.idx_pad)
 
         if self.report_every and self.optScheduler._step % self.report_every == 0: ### report
-          loss_per_tok, ms_per_step = score.report()
-          logging.info('Learning step: {} epoch: {} batch: {} steps/sec: {:.2f} lr: {:.6f} loss: {:.3f}'.format(self.optScheduler._step, n_epoch, n_batch, 1000.0/ms_per_step, self.optScheduler._rate, loss_per_tok))
+          acc_per_tok, loss_per_tok, ms_per_step = score.report()
+          logging.info('Learning step: {} epoch: {} batch: {} steps/sec: {:.2f} lr: {:.6f} Loss: {:.3f} Acc: {:.3f}'.format(self.optScheduler._step, n_epoch, n_batch, 1000.0/ms_per_step, self.optScheduler._rate, loss_per_tok, acc_per_tok))
           #self.writer.add_scalar('Loss/train', loss_per_tok, self.optScheduler._step)
           self.writer.add_scalar('Loss/train', loss_token.item(), self.optScheduler._step)
           self.writer.add_scalar('LearningRate', self.optScheduler._rate, self.optScheduler._step)
@@ -135,8 +159,8 @@ class Learning():
           logging.info('Learning STOP by [steps={}]'.format(self.optScheduler._step))
           return
 
-      loss_per_tok, ms_epoch = score.epoch()
-      logging.info('EndOfEpoch: {} #batchs: {} loss: {:.3f} sec: {:.2f}'.format(n_epoch,n_batch,loss_per_tok,ms_epoch/1000.0))
+      acc_per_tok, loss_per_tok, ms_epoch = score.epoch()
+      logging.info('EndOfEpoch: {} #batchs: {} Loss: {:.3f} Acc: {:.3f} sec: {:.2f}'.format(n_epoch,n_batch,loss_per_tok,acc_per_tok,ms_epoch/1000.0))
 
       if self.max_epochs and n_epoch >= self.max_epochs: ### stop by max_epochs
         if validset is not None:
