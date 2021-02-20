0b2760471a9a10b851049638b6aefb9f46806020
jmcrego <jmcrego@gmail.com>
2021-02-20 01:27:45 +0100

    light

--- transformer/Dataset.py
+++ transformer/Dataset.py
@@ -158,7 +158,7 @@ class Dataset():
     perc_tgt = 100.0*n_tgt_unks/n_tgt_tokens if n_tgt_tokens else 0.0
     avg_lsrc = 1.0*n_src_tokens/len(idxs_pos)
     avg_ltgt = 1.0*n_tgt_tokens/len(idxs_pos)
-    logging.info('Built shard with {} examples ~ {}:{} tokens ~ {}:{} OOVs [{:.2f}%:{:.2f}%] ~ {} filtered examples ~ average sentence length {:.2f}:{:.2f} ~ {}:{}'.format(len(idxs_pos), n_src_tokens, n_tgt_tokens, n_src_unks, n_tgt_unks, perc_src, perc_tgt, n_filtered, avg_lsrc, avg_ltgt, self.ftxt_src, self.ftxt_tgt))
+    logging.info('Built shard with {} examples ~ {}:{} tokens ~ {}:{} OOVs [{:.2f}%:{:.2f}%] ~ {} filtered examples ~ average sentence length {:.1f}:{:.1f} ~ {}:{}'.format(len(idxs_pos), n_src_tokens, n_tgt_tokens, n_src_unks, n_tgt_unks, perc_src, perc_tgt, n_filtered, avg_lsrc, avg_ltgt, self.ftxt_src, self.ftxt_tgt))
     return idxs_len, idxs_pos
 
 
