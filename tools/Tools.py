c32c8c6b06c350d8abbebb2aecb32a7ec5803aac
jmcrego <jmcrego@gmail.com>
2021-02-20 19:01:21 +0100

    read_dnet

--- tools/Tools.py
+++ tools/Tools.py
@@ -27,3 +27,41 @@ def isbinary(fin):
       return False
   except UnicodeDecodeError: # Fond non-text data  
     return True
+
+def read_dnet(dnet):
+  if not os.path.isdir(dnet):
+    logging.error('unavailable network directory: {}'.format(dnet))
+    sys.exit()
+  if not os.path.isfile(dnet + '/network'):
+    logging.error('cannot find network file: {}'.format(dnet + '/network'))
+    sys.exit()
+  if not os.path.isfile(dnet + '/joint_pre') and (not os.path.isfile(dnet + '/src_pre') or not os.path.isfile(dnet + '/tgt_pre')):
+    logging.error('cannot find preprocessor file/s')
+    sys.exit()
+
+  with open(dnet + '/network', 'r') as f:
+    n = yaml.load(f, Loader=yaml.SafeLoader) 
+  logging.info("Network = {}".format(n))
+
+  if os.path.isfile(dnet + '/joint_pre'):
+    if isbinary(dnet + '/joint_pre'): 
+      src_pre = SentencePiece(fmod=dnet + '/joint_pre')
+      tgt_pre = src_pre
+    else: 
+      src_pre = Space(fmod=dnet + '/joint_pre')
+      tgt_pre = src_pre
+
+  else: ### src_pre / tgt_pre
+    if isbinary(dnet + '/src_pre'): 
+      src_pre = SentencePiece(fmod=dnet + '/src_pre')
+      tgt_pre = SentencePiece(fmod=dnet + '/tgt_pre')
+    else: 
+      src_pre = Space(fmod=dnet + '/src_pre')
+      tgt_pre = Space(fmod=dnet + '/tgt_pre')
+
+  assert src_pre.idx_pad == tgt_pre.idx_pad, 'src/tgt vocabularies must have the same idx_pad'
+  return n, src_pre, tgt_pre
+
+
+
+
