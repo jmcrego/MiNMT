c4da2cfc2b1011410bea50af6980a6580e6e4c4a
jmcrego <jmcrego@gmail.com>
2021-03-04 23:01:11 +0100

    light

--- transformer/Learning.py
+++ transformer/Learning.py
@@ -127,29 +127,35 @@ class Learning():
       for batch_pos, batch_idxs in trainset:
         batch_src = batch_idxs[0]
         batch_tgt = batch_idxs[1]
-        logging.info("batch pos={}".format(' '.join(map(str,batch_pos))))
-        continue                 
         n_batch += 1
         self.model.train()
+        ###
         ### forward
+        ###
         src, msk_src = prepare_source(batch_src, self.idx_pad, device)
         tgt, ref, msk_tgt = prepare_target(batch_tgt, self.idx_pad, device)
         pred = self.model.forward(src, tgt, msk_src, msk_tgt) #no log_softmax is applied
+        ###
         ### compute loss
+        ###
         loss_batch = self.criter(pred, ref) #sum of losses in batch
         ntok_batch = torch.sum(ref != self.idx_pad)
         loss_token = loss_batch / ntok_batch
-
+        ###
         ### optimize
+        ###
         self.optScheduler.optimizer.zero_grad()                                        ### sets gradients to zero
         loss_token.backward()
         if self.clip_grad_norm > 0.0:
           torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.clip_grad_norm) ### clip gradients to clip_grad_norm
         self.optScheduler.step()                                                       ### updates model parameters after incrementing step and updating lr
+        ###
         ### accumulate score
+        ###
         score.step(loss_batch.item(), ntok_batch, ref, pred, self.idx_pad)
-
+        ###
         ### report
+        ###
         if self.report_every and self.optScheduler._step % self.report_every == 0: 
           #acc_per_tok, loss_per_tok, ms_per_step = score.report()
           #logging.info('Learning step: {} epoch: {} batch: {} steps/sec: {:.2f} lr: {:.6f} Acc: {:.3f} Loss: {:.3f}'.format(self.optScheduler._step, n_epoch, n_batch, 1000.0/ms_per_step, self.optScheduler._rate, acc_per_tok, loss_per_tok))
@@ -159,14 +165,20 @@ class Learning():
           if tensorboard:
             self.writer.add_scalar('Loss/train', loss_token.item(), self.optScheduler._step)
             self.writer.add_scalar('LearningRate', self.optScheduler._rate, self.optScheduler._step)
+        ###
         ### validate
+        ###
         if self.validate_every and self.optScheduler._step % self.validate_every == 0: 
           if validset is not None:
             vloss = self.validate(validset, device)
+        ###
         ### save
+        ###
         if self.save_every and self.optScheduler._step % self.save_every == 0: 
           save_checkpoint(self.suffix, self.model, self.optScheduler.optimizer, self.optScheduler._step, self.keep_last_n)
+        ###
         ### stop by max_steps
+        ###
         if self.max_steps and self.optScheduler._step >= self.max_steps: 
           if validset is not None:
             vloss = self.validate(validset, device)
@@ -178,14 +190,15 @@ class Learning():
       #logging.info('EndOfEpoch: {} #batchs: {} Acc: {:.3f} Loss: {:.3f} sec: {:.2f}'.format(n_epoch,n_batch,acc_per_tok,loss_per_tok,ms_epoch/1000.0))
       loss_per_tok, ms_epoch = score.epoch()
       logging.info('EndOfEpoch: {} #batchs: {} Loss: {:.3f} sec: {:.2f}'.format(n_epoch,n_batch,loss_per_tok,ms_epoch/1000.0))
-
+      ###
+      ### stop by max_epochs
+      ###
       if self.max_epochs and n_epoch >= self.max_epochs: ### stop by max_epochs
         if validset is not None:
           vloss = self.validate(validset, device)
         save_checkpoint(self.suffix, self.model, self.optScheduler.optimizer, self.optScheduler._step, self.keep_last_n)
         logging.info('Learning STOP by [epochs={}]'.format(n_epoch))
         return
-    return
 
   def validate(self, validset, device):
     tic = time.time()
