d4ba53b504094189b216bd72dd591577796e923b
jmcrego <jmcrego@gmail.com>
2021-03-06 10:21:58 +0100

    setup load/save models

--- minmt-train.py
+++ minmt-train.py
@@ -176,16 +176,19 @@ if __name__ == '__main__':
   src_voc = Vocab(src_voc)
   tgt_voc = Vocab(tgt_voc)
 
-  #############################
-  ### load model/optim/loss ###
-  #############################
+  ########################
+  ### load model/optim ###
+  ########################
   device = torch.device('cuda' if o.cuda and torch.cuda.is_available() else 'cpu')
   model = Encoder_Decoder(n['n_layers'], n['ff_dim'], n['n_heads'], n['emb_dim'], n['qk_dim'], n['v_dim'], n['dropout'], n['share_embeddings'], len(src_voc), len(tgt_voc), src_voc.idx_pad).to(device)
   logging.info('Built model (#params, size) = ({}) in device {}'.format(', '.join([str(f) for f in numparameters(model)]), next(model.parameters()).device ))
   optim = torch.optim.Adam(model.parameters(), weight_decay=n['weight_decay'], betas=(n['beta1'], n['beta2']), eps=n['eps']) 
   last_step, model, optim = load_checkpoint_or_initialise(o.dnet + '/network', model, optim, device)
-  optScheduler = OptScheduler(optim, n['emb_dim'], o.noam_scale, o.noam_warmup, last_step)
 
+  ############################
+  ### build scheduler/loss ###
+  ############################
+  optScheduler = OptScheduler(optim, n['emb_dim'], o.noam_scale, o.noam_warmup, last_step)
   if o.loss == 'KLDiv':
     criter = LabelSmoothing_KLDiv(len(tgt_voc), src_voc.idx_pad, o.label_smoothing).to(device)
   elif o.loss == 'NLL':
