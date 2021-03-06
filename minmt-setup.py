d962f599cfe356420fa89e31251b5a242005aea8
jmcrego <jmcrego@gmail.com>
2021-03-06 10:18:38 +0100

    setup load/save models

--- minmt-setup.py
+++ minmt-setup.py
@@ -17,24 +17,24 @@ import numpy as np
 class Options():
   def __init__(self, argv):
     self.prog = argv.pop(0)
-
+    self.dnet = None
     self.src_voc = None
     self.tgt_voc = None
 
-    self.emb_dim = 512
-    self.qk_dim = 64
-    self.v_dim = 64
-    self.ff_dim = 2048
-    self.n_heads = 8
-    self.n_layers = 6
-    self.dropout = 0.1
-    self.share_embeddings = False
-    self.dnet = None
+    self.n = {}
+    self.n['emb_dim'] = 512
+    self.n['qk_dim'] = 64
+    self.n['v_dim'] = 64
+    self.n['ff_dim'] = 2048
+    self.n['n_heads'] = 8
+    self.n['n_layers'] = 6
+    self.n['dropout'] = 0.1
+    self.n['share_embeddings'] = False
 
-    self.weight_decay = 0.0
-    self.beta1 = 0.9
-    self.beta2 = 0.998
-    self.eps = 1e-9
+    self.n['weight_decay'] = 0.0
+    self.n['beta1'] = 0.9
+    self.n['beta2'] = 0.998
+    self.n['eps'] = 1e-9
 
     log_file = 'stderr'
     log_level = 'info'
@@ -52,30 +52,30 @@ class Options():
         self.tgt_voc = argv.pop(0)
 
       elif tok=="-emb_dim" and len(argv):
-        self.emb_dim = int(argv.pop(0))
+        self.n['emb_dim'] = int(argv.pop(0))
       elif tok=="-qk_dim" and len(argv):
-        self.qk_dim = int(argv.pop(0))
+        self.n['qk_dim'] = int(argv.pop(0))
       elif tok=="-v_dim" and len(argv):
-        self.v_dim = int(argv.pop(0))
+        self.n['v_dim'] = int(argv.pop(0))
       elif tok=="-ff_dim" and len(argv):
-        self.ff_dim = int(argv.pop(0))
+        self.n['ff_dim'] = int(argv.pop(0))
       elif tok=="-n_heads" and len(argv):
-        self.n_heads = int(argv.pop(0))
+        self.n['n_heads'] = int(argv.pop(0))
       elif tok=="-n_layers" and len(argv):
-        self.n_layers = int(argv.pop(0))
+        self.n['n_layers'] = int(argv.pop(0))
       elif tok=="-dropout" and len(argv):
-        self.dropout = float(argv.pop(0))
+        self.n['dropout'] = float(argv.pop(0))
       elif tok=="-share_embeddings":
-        self.share_embeddings = True
+        self.n['share_embeddings'] = True
 
       elif tok=='-weight_decay':
-        self.weight_decay= float(argv.pop(0))
+        self.n['weight_decay'] = float(argv.pop(0))
       elif tok=='-beta1':
-        self.beta1 = float(argv.pop(0))
+        self.n['beta1'] = float(argv.pop(0))
       elif tok=='-beta2':
-        self.beta2 = float(argv.pop(0))
+        self.n['beta2'] = float(argv.pop(0))
       elif tok=='-eps':
-        self.eps = float(argv.pop(0))
+        self.n['eps'] = float(argv.pop(0))
 
       elif tok=="-log_file" and len(argv):
         log_file = argv.pop(0)
@@ -94,7 +94,7 @@ class Options():
       self.usage()
 
   def usage(self):
-    sys.stderr.write('''usage: {} -dnet DIR [Options]
+    sys.stderr.write('''usage: {} -dnet DIR -src_voc FILE -tgt_voc FILE [Options]
    -dnet            DIR : network ouput directory [must not exist]
    -src_voc        FILE : source vocabulary file
    -tgt_voc        FILE : target vocabulary file
@@ -108,15 +108,15 @@ class Options():
    -dropout       FLOAT : dropout value ({})
    -share_embeddings    : share source/target embeddings ({})
 
-   -weight_decay    FLOAT : Adam optimizer weight decay ({})
-   -beta1           FLOAT : beta1 for Adam optimizer ({})
-   -beta2           FLOAT : beta2 for Adam optimizer ({})
-   -eps             FLOAT : epsilon for Adam optimizer ({})
+   -weight_decay  FLOAT : weight decay for Adam optimizer ({})
+   -beta1         FLOAT : beta1 for Adam optimizer ({})
+   -beta2         FLOAT : beta2 for Adam optimizer ({})
+   -eps           FLOAT : epsilon for Adam optimizer ({})
 
-   -log_file    FILE : log file  (stderr)
-   -log_level    STR : log level [debug, info, warning, critical, error] (info)
-   -h                : this help
-'''.format(self.prog, self.emb_dim, self.qk_dim, self.v_dim, self.ff_dim, self.n_heads, self.n_layers, self.dropout, self.share_embeddings, self.weight_decay, self.beta1, self.beta2, self.eps))
+   -log_file       FILE : log file  (stderr)
+   -log_level       STR : log level [debug, info, warning, critical, error] (info)
+   -h                   : this help
+'''.format(self.prog, self.n['emb_dim'], self.n['qk_dim'], self.n['v_dim'], self.n['ff_dim'], self.n['n_heads'], self.n['n_layers'], self.n['dropout'], self.n['share_embeddings'], self.n['weight_decay'], self.n['beta1'], self.n['beta2'], self.n['eps']))
     sys.exit()
 
 ######################################################################
@@ -127,15 +127,15 @@ if __name__ == '__main__':
 
   tic = time.time()
   o = Options(sys.argv)
+  write_dnet(o) ### saves network, src_voc, tgt_voc
   src_voc = Vocab(o.src_voc)
   tgt_voc = Vocab(o.tgt_voc)
   device = torch.device('cpu')
-  model = Encoder_Decoder(o.n_layers, o.ff_dim, o.n_heads, o.emb_dim, o.qk_dim, o.v_dim, o.dropout, o.share_embeddings, len(src_voc), len(tgt_voc), src_voc.idx_pad).to(device)
+  model = Encoder_Decoder(o.n['n_layers'], o.n['ff_dim'], o.n['n_heads'], o.n['emb_dim'], o.n['qk_dim'], o.n['v_dim'], o.n['dropout'], o.n['share_embeddings'], len(src_voc), len(tgt_voc), src_voc.idx_pad).to(device)
   logging.info('Built model (#params, size) = ({}) in device {}'.format(', '.join([str(f) for f in numparameters(model)]), next(model.parameters()).device ))
   model = initialise(model)
-  optim = torch.optim.Adam(model.parameters(), weight_decay=o.weight_decay, betas=(o.beta1, o.beta2), eps=o.eps) 
-  write_dnet(o) ### writes network, src_voc, tgt_voc
-  save_checkpoint(o.dnet+ '/network', model, optim, 0, 0)
+  optim = torch.optim.Adam(model.parameters(), weight_decay=o.n['weight_decay'], betas=(o.n['beta1'], o.n['beta2']), eps=o.n['eps']) 
+  save_checkpoint(o.dnet+ '/network', model, optim, 0, 0) ### saves model checkpoint and optimizer
   toc = time.time()
   logging.info('Done ({:.2f} seconds)'.format(toc-tic))
 
--- tools/Tools.py
+++ tools/Tools.py
@@ -17,21 +17,6 @@ def create_logger(logfile, loglevel):
     logging.basicConfig(filename=logfile, format='[%(asctime)s.%(msecs)03d] %(levelname)s %(message)s', datefmt='%Y-%m-%d_%H:%M:%S', level=numeric_level)
     logging.debug('Created Logger level={} file={}'.format(loglevel, logfile))
 
-def getValue(s):
-  try:
-    return int(s)
-  except ValueError:
-    pass
-  try:
-    return float(s)
-  except ValueError:
-    pass
-  if s=='True':
-    return True
-  if s=='False':
-    return False
-  return s
-
 def read_dnet(dnet):
   if not os.path.isdir(dnet):
     logging.error('unavailable network directory: {}'.format(dnet))
@@ -39,31 +24,18 @@ def read_dnet(dnet):
   if not os.path.isfile(dnet + '/network'):
     logging.error('cannot find network file: {}'.format(dnet + '/network'))
     sys.exit()
-  if not os.path.isfile(dnet + '/joint_voc') and (not os.path.isfile(dnet + '/src_voc') or not os.path.isfile(dnet + '/tgt_voc')):
+  if not os.path.isfile(dnet + '/src_voc') or not os.path.isfile(dnet + '/tgt_voc'):
     logging.error('cannot find vocabulary file/s')
     sys.exit()
 
   with open(dnet + '/network', 'r') as f:
-    n = {}
-    for l in f:
-      toks = l.split()
-      if len(toks) == 2:
-        key = toks[0][:-1] #discard ':'
-        val = getValue(toks[1])
-        #print(key,val)
-        n[key] = val        
-      else:
-        logging.error('Bad network option line: {}'.format(l))
-        sys.exit()
+    s = f.read()
+  n = eval(s)
 
   logging.info("Network = {}".format(n))
 
-  if os.path.isfile(dnet + '/joint_voc'): ### joint voc
-    src_voc = dnet + '/joint_voc'
-    tgt_voc = src_voc
-  else: ### src_voc / tgt_voc
-    src_voc = dnet + '/src_voc'
-    tgt_voc = dnet + '/tgt_voc'
+  src_voc = dnet + '/src_voc'
+  tgt_voc = dnet + '/tgt_voc'
 
   return n, src_voc, tgt_voc
 
@@ -82,28 +54,14 @@ def write_dnet(o):
   os.mkdir(o.dnet)
   logging.info('created network directory: {}'.format(o.dnet))
 
-  with open(o.dnet+'/network', 'w') as f:
-    f.write('emb_dim: {}\n'.format(o.emb_dim))
-    f.write('qk_dim: {}\n'.format(o.qk_dim))
-    f.write('v_dim: {}\n'.format(o.v_dim))
-    f.write('ff_dim: {}\n'.format(o.ff_dim))
-    f.write('n_heads: {}\n'.format(o.n_heads))
-    f.write('n_layers: {}\n'.format(o.n_layers))
-    f.write('dropout: {}\n'.format(o.dropout))
-    f.write('share_embeddings: {}\n'.format(o.share_embeddings))
-    f.write('weight_decay: {}\n'.format(o.weight_decay))
-    f.write('beta1: {}\n'.format(o.beta1))
-    f.write('beta2: {}\n'.format(o.beta2))
-    f.write('eps: {}\n'.format(o.eps))
+  with open(o.dnet + '/network', 'w') as f:
+    f.write(str(o.n))
 
-  if o.src_voc == o.tgt_voc: ### joint voc
-    shutil.copy(o.src_voc, o.dnet+'/joint_voc')
-    logging.info('copied source/target vocabulary {} into {}/joint_voc'.format(o.src_voc, o.dnet))
-  else: ### src_voc / tgt_voc
-    shutil.copy(o.src_voc, o.dnet+'/src_voc')
-    logging.info('copied source vocabulary {} into {}/src_voc'.format(o.src_voc, o.dnet))
-    shutil.copy(o.tgt_voc, o.dnet+'/tgt_voc')
-    logging.info('copied target vocabulary {} into {}/tgt_voc'.format(o.tgt_voc, o.dnet))
+  shutil.copy(o.src_voc, o.dnet+'/src_voc')
+  logging.info('copied source vocabulary {} into {}/src_voc'.format(o.src_voc, o.dnet))
+
+  shutil.copy(o.tgt_voc, o.dnet+'/tgt_voc')
+  logging.info('copied target vocabulary {} into {}/tgt_voc'.format(o.tgt_voc, o.dnet))
 
 def flatten_count(ll, count):
   lflat = []
