f20dc0a4ba4be522d98d038cb2a3a43b2e5d1793
jmcrego <jmcrego@gmail.com>
2021-05-06 22:21:54 +0200

    s_s_scc

--- transformer/Model_s_s_scc.py
+++ transformer/Model_s_s_scc.py
@@ -7,7 +7,7 @@ import torch
 import math
 import numpy as np
 import glob
-from transformer.Model import Embedding, AddPositionalEncoding, Stacked_Encoder, Stacked_Decoder, Encoder, Decoder, MultiHead_Attn, FeedForward, Generator
+from transformer.Model import Embedding, AddPositionalEncoding, Stacked_Encoder, Stacked_Decoder, Encoder, Decoder, Stacked_Decoder_scc, Decoder_scc, MultiHead_Attn, FeedForward, Generator
 
 ##############################################################################################################
 ### Encoder_Decoder_s_s_scc ##################################################################################
@@ -68,64 +68,5 @@ class Encoder_Decoder_s_s_scc(torch.nn.Module):
     y = self.generator_trn(z_tgt) #[bs, lt, Vt]
     y = torch.nn.functional.log_softmax(y, dim=-1) 
     return y ### returns log_probs (for inference)
-
-##############################################################################################################
-### Stacked_Decoder_scc ######################################################################################
-##############################################################################################################
-class Stacked_Decoder_scc(torch.nn.Module):
-  def __init__(self, n_layers, ff_dim, n_heads, emb_dim, qk_dim, v_dim, dropout):
-    super(Stacked_Decoder_scc, self).__init__()
-    self.decoderlayers = torch.nn.ModuleList([Decoder_scc(ff_dim, n_heads, emb_dim, qk_dim, v_dim, dropout) for _ in range(n_layers)])
-    self.norm = torch.nn.LayerNorm(emb_dim, eps=1e-6) 
-
-  def forward(self, z_src, z_xtgt, tgt, msk_src, msk_xtgt, msk_tgt):
-    for i,decoderlayer in enumerate(self.decoderlayers):
-      tgt = decoderlayer(z_src, z_xtgt, tgt, msk_src, msk_xtgt, msk_tgt)
-    return self.norm(tgt)
-
-##############################################################################################################
-### Decoder_scc ##############################################################################################
-##############################################################################################################
-class Decoder_scc(torch.nn.Module):
-  def __init__(self, ff_dim, n_heads, emb_dim, qk_dim, v_dim, dropout):
-    super(Decoder_scc, self).__init__()
-    self.multihead_attn_self = MultiHead_Attn(n_heads, emb_dim, qk_dim, v_dim, dropout)
-    self.multihead_attn_cross1 = MultiHead_Attn(n_heads, emb_dim, qk_dim, v_dim, dropout)
-    self.multihead_attn_cross2 = MultiHead_Attn(n_heads, emb_dim, qk_dim, v_dim, dropout)
-    self.feedforward = FeedForward(emb_dim, ff_dim, dropout)
-    self.norm_att_self = torch.nn.LayerNorm(emb_dim, eps=1e-6) 
-    self.norm_att_cross1 = torch.nn.LayerNorm(emb_dim, eps=1e-6) 
-    self.norm_att_cross2 = torch.nn.LayerNorm(emb_dim, eps=1e-6) 
-    self.norm_ff = torch.nn.LayerNorm(emb_dim, eps=1e-6) 
-
-  def forward(self, z_src, z_xtgt, tgt, msk_src, msk_xtgt, msk_tgt):
-    #NORM
-    tmp_norm = self.norm_att_self(tgt)
-    #Self ATTN over tgt (previous) words : q, k, v are tgt words
-    tmp2 = self.multihead_attn_self(q=tmp_norm, k=tmp_norm, v=tmp_norm, msk=msk_tgt) #[bs, lt, ed] contains dropout
-    #ADD
-    tmp = tmp2 + tgt 
-
-    #NORM
-    tmp_norm = self.norm_att_cross1(tmp)
-    #Cross ATTN over xtgt words : q are tgt words, k, v are xtgt words
-    tmp2 = self.multihead_attn_cross1(q=tmp_norm, k=z_xtgt, v=z_xtgt, msk=msk_xtgt) #[bs, lt, ed] contains dropout
-    #ADD
-    tmp = tmp2 + tmp
-
-    #NORM
-    tmp_norm = self.norm_att_cross2(tmp)
-    #Cross ATTN over src words : q are tgt words, k, v are src words
-    tmp2 = self.multihead_attn_cross2(q=tmp_norm, k=z_src, v=z_src, msk=msk_src) #[bs, lt, ed] contains dropout
-    #ADD
-    tmp = tmp2 + tmp
-
-    #NORM
-    tmp_norm = self.norm_ff(tmp)
-    #FF
-    tmp2 = self.feedforward(tmp_norm) #[bs, lt, ed] contains dropout
-    #ADD
-    z = tmp2 + tmp
-    return z
 
 
