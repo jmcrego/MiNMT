e243e7a254b42af4072f47770a414ba06258d0f7
jmcrego <jmcrego@gmail.com>
2021-05-31 14:08:38 +0200

    light

--- transformer/Inference.py
+++ transformer/Inference.py
@@ -65,8 +65,7 @@ class Inference():
           src, self.msk_src = prepare_source(batch_idxs[0], self.src_voc.idx_pad, self.device) #src is [bs, ls] msk_src is [bs,1,ls]
           xsrc, msk_xsrc = prepare_source(batch_idxs[1], self.src_voc.idx_pad, self.device) #xsrc is [bs, lxs] msk_xsrc is [bs,1,lxs]
           xtgt, self.msk_xtgt = prepare_source(batch_idxs[2], self.tgt_voc.idx_pad, self.device) #xtgt is [bs, lxt] msk_xtgt is [bs,1,lxt]
-        self.z_src, self.z_xtgt = self.model.encode(src, xsrc, xtgt, self.msk_src, msk_xsrc, self.msk_xtgt) #[bs,ls,ed] [bs,lxt,ed]
-
+          self.z_src, self.z_xtgt = self.model.encode(src, xsrc, xtgt, self.msk_src, msk_xsrc, self.msk_xtgt) #[bs,ls,ed] [bs,lxt,ed]
         elif self.model_type == 'sxs_sc':
           src, self.msk_src = prepare_source(batch_idxs[0], self.src_voc.idx_pad, self.device) #src is [bs, ls] msk_src is [bs,1,ls]
           xtgt, self.msk_xtgt = prepare_source(batch_idxs[1], self.tgt_voc.idx_pad, self.device) #xtgt is [bs, lxt] msk_xtgt is [bs,1,lxt]
