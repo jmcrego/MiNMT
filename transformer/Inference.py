419fb0d159ce58725d69f39875171941edeea80e
jmcrego <jmcrego@gmail.com>
2021-05-31 13:07:47 +0200

    light

--- transformer/Inference.py
+++ transformer/Inference.py
@@ -61,6 +61,12 @@ class Inference():
           xtgt, self.msk_xtgt = prepare_source(batch_idxs[2], self.tgt_voc.idx_pad, self.device) #xtgt is [bs, lxt] msk_xtgt is [bs,1,lxt]
           self.z_src, self.z_xtgt = self.model.encode(src, xsrc, xtgt, self.msk_src, msk_xsrc, self.msk_xtgt) #[bs,ls,ed] [bs,lxt,ed]
 
+        elif self.model_type == '2nmt_2c':
+          src, self.msk_src = prepare_source(batch_idxs[0], self.src_voc.idx_pad, self.device) #src is [bs, ls] msk_src is [bs,1,ls]
+          xsrc, msk_xsrc = prepare_source(batch_idxs[1], self.src_voc.idx_pad, self.device) #xsrc is [bs, lxs] msk_xsrc is [bs,1,lxs]
+          xtgt, self.msk_xtgt = prepare_source(batch_idxs[2], self.tgt_voc.idx_pad, self.device) #xtgt is [bs, lxt] msk_xtgt is [bs,1,lxt]
+          self.z_src, self.z_xtgt = self.model.encode(src, xsrc, xtgt, self.msk_src, msk_xsrc, self.msk_xtgt) #[bs,ls,ed] [bs,lxt,ed]
+
         elif self.model_type == 'sxs_sc':
           src, self.msk_src = prepare_source(batch_idxs[0], self.src_voc.idx_pad, self.device) #src is [bs, ls] msk_src is [bs,1,ls]
           xtgt, self.msk_xtgt = prepare_source(batch_idxs[1], self.tgt_voc.idx_pad, self.device) #xtgt is [bs, lxt] msk_xtgt is [bs,1,lxt]
@@ -120,6 +126,11 @@ class Inference():
           self.msk_src = self.msk_src.repeat_interleave(repeats=self.K, dim=0) #[bs,1,ls] => [bs*K,1,ls]
           self.z_xtgt = self.z_xtgt.repeat_interleave(repeats=self.K, dim=0) #[bs,lxt,ed] => [bs*K,lxt,ed]
           self.msk_xtgt = self.msk_xtgt.repeat_interleave(repeats=self.K, dim=0) #[bs,1,lxt] => [bs*K,1,lxt]
+        elif self.model_type == '2nmt_2c':
+          self.z_src = self.z_src.repeat_interleave(repeats=self.K, dim=0) #[bs,ls,ed] => [bs*K,ls,ed]
+          self.msk_src = self.msk_src.repeat_interleave(repeats=self.K, dim=0) #[bs,1,ls] => [bs*K,1,ls]
+          self.z_xtgt = self.z_xtgt.repeat_interleave(repeats=self.K, dim=0) #[bs,lxt,ed] => [bs*K,lxt,ed]
+          self.msk_xtgt = self.msk_xtgt.repeat_interleave(repeats=self.K, dim=0) #[bs,1,lxt] => [bs*K,1,lxt]
         elif self.model_type == 'sxs_sc':
           self.z_src = self.z_src.repeat_interleave(repeats=self.K, dim=0) #[bs,ls,ed] => [bs*K,ls,ed]
           self.msk_src = self.msk_src.repeat_interleave(repeats=self.K, dim=0) #[bs,1,ls] => [bs*K,1,ls]
@@ -143,6 +154,8 @@ class Inference():
       if self.model_type == 's_sc':
         y_next = self.model.decode(self.z_src, hyps, self.msk_src, msk_tgt=msk_tgt)[:,-1,:] #[I,lt,Vt] => [I,Vt]
       elif self.model_type == 's_s_scc_scc':
+        y_next = self.model.decode(self.z_src, self.z_xtgt, hyps, self.msk_src, self.msk_xtgt, msk_tgt=msk_tgt)[:,-1,:] #[I,lt,Vt] => [I,Vt]
+      elif self.model_type == '2nmt_2c':
         y_next = self.model.decode(self.z_src, self.z_xtgt, hyps, self.msk_src, self.msk_xtgt, msk_tgt=msk_tgt)[:,-1,:] #[I,lt,Vt] => [I,Vt]
       elif self.model_type == 'sxs_sc':
         y_next = self.model.decode(self.z_src, self.z_xtgt, hyps, self.msk_src, self.msk_xtgt, msk_tgt=msk_tgt)[:,-1,:] #[I,lt,Vt] => [I,Vt]
